c52e11c7826ebe428b7bff8cea5dbd27d0228167
Dmitry <kuchura.d@gmail.com>
2017-10-13 20:28:08 +0300

    Update companies.php and users.php

--- components/views/companies.php
+++ components/views/companies.php
@@ -1,28 +1,28 @@
 <?php /* @var $result Companies */ ?>
 <?php echo CHtml::link('Add / Create', '#', ['class' => 'btn btn-success modal-btn', 'data-target' => 'api/company-create']); ?>
-    <hr>
+<hr>
+<ul id="companyData" class="list-group usersData">
 <?php if ($result): ?>
-    <ul id="companyData" class="list-group usersData">
-        <?php foreach ($result as $obj): ?>
-            <li class="list-group-item usersData-item">
-                <div class="usersData-item__text"
-                     id="<?php echo 'company-' . $obj->id; ?>"><?php echo $obj->name . ' / ' . Helper::fileSizeConvert($obj->quota); ?></div>
-                <div class="usersData-item__buttons">
-                    <button class="btn btn-info modal-btn"
-                            data-target="<?php echo Yii::app()->createUrl('api/company-update', ['id' => $obj->id]); ?>">
-                        Edit
-                    </button>
-                    <button class="btn btn-danger deleteButton"
-                            data-table="companies"
-                            data-id="<?php echo $obj->id; ?>">
-                        Delete
-                    </button>
-                </div>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <?php foreach ($result as $obj): ?>
+        <li class="list-group-item usersData-item">
+            <div class="usersData-item__text"
+                 id="<?php echo 'company-' . $obj->id; ?>"><?php echo $obj->name . ' / ' . Helper::fileSizeConvert($obj->quota); ?></div>
+            <div class="usersData-item__buttons">
+                <button class="btn btn-info modal-btn"
+                        data-target="<?php echo Yii::app()->createUrl('api/company-update', ['id' => $obj->id]); ?>">
+                    Edit
+                </button>
+                <button class="btn btn-danger deleteButton"
+                        data-table="companies"
+                        data-id="<?php echo $obj->id; ?>">
+                    Delete
+                </button>
+            </div>
+        </li>
+    <?php endforeach; ?>
 <?php else: ?>
     <div class="alert alert-warning" role="alert">
         <p>No data companies to view. Please create company click to <strong>Add / Create</strong> button.</p>
     </div>
 <?php endif; ?>
+</ul>
--- components/views/users.php
+++ components/views/users.php
@@ -1,30 +1,30 @@
 <?php /* @var $result Users */ ?>
 
 <?php echo CHtml::link('Add / Create', '#', ['class' => 'btn btn-success modal-btn', 'data-target' => 'api/user-create']); ?>
-    <hr>
+<hr>
+<ul id="usersData" class="list-group usersData">
 <?php if ($result): ?>
-    <ul id="usersData" class="list-group usersData">
-        <?php foreach ($result as $obj): ?>
-            <li class="list-group-item usersData-item">
-                <div class="usersData-item__text" id="<?php echo 'user-' . $obj->id; ?>">
-                    <?php echo $obj->name . ' / ' . $obj->email . ' / '; ?>
-                    <?php echo isset($obj->company->id) ? $obj->company->name : 'Company deleted!'; ?>
-                </div>
-                <div class="usersData-item__buttons">
-                    <button class="btn btn-info modal-btn"
-                            data-target="<?php echo Yii::app()->createUrl('api/user-update', ['id' => $obj->id]); ?>">
-                        Edit
-                    </button>
-                    <button class="btn btn-danger deleteButton"
-                            data-table="users" data-id="<?php echo $obj->id; ?>">
-                        Delete
-                    </button>
-                </div>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <?php foreach ($result as $obj): ?>
+        <li class="list-group-item usersData-item">
+            <div class="usersData-item__text" id="<?php echo 'user-' . $obj->id; ?>">
+                <?php echo $obj->name . ' / ' . $obj->email . ' / '; ?>
+                <?php echo isset($obj->company->id) ? $obj->company->name : 'Company deleted!'; ?>
+            </div>
+            <div class="usersData-item__buttons">
+                <button class="btn btn-info modal-btn"
+                        data-target="<?php echo Yii::app()->createUrl('api/user-update', ['id' => $obj->id]); ?>">
+                    Edit
+                </button>
+                <button class="btn btn-danger deleteButton"
+                        data-table="users" data-id="<?php echo $obj->id; ?>">
+                    Delete
+                </button>
+            </div>
+        </li>
+    <?php endforeach; ?>
 <?php else: ?>
     <div class="alert alert-warning" role="alert">
         <p>No data user to view. Please create user click to <strong>Add / Create</strong> button.</p>
     </div>
 <?php endif; ?>
+</ul>
